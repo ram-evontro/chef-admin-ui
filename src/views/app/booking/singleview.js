462ea2e279e9c5fe3db6e827f0d37b37e7d7c6a3
raghuwar-jangir <raghujangid222@gmail.com>
2023-10-05 12:59:52 +0530

    date issue solved

--- src/views/app/booking/singleview.js
+++ src/views/app/booking/singleview.js
@@ -376,7 +376,7 @@ const Singleview = ({ match, history }) => {
                       <b>Diner Count:</b> {booking.diner_count}
                     </p>
                     <p>
-                      <b>Experience date:</b> {moment.utc(booking.booking_date).format('MMM D, YYYY')}
+                      <b>Experience date:</b> {moment(booking.booking_date).format('MMM D, YYYY')}
                       {booking.dunzo_taskids && booking.dunzo_taskids.length === 0 && booking.type === "virtual_dining" ? (
                         <button onClick={updateBookingTime} className="btn btn-outline-primary" title="Edit delivery Time">
                           <i className="simple-icon-pencil" />
@@ -386,7 +386,7 @@ const Singleview = ({ match, history }) => {
                       )}
                     </p>
                     <p>
-                      <b>Slot:</b> {booking.slot}
+                      <b>Slot:</b> {booking.slot || "10:00 AM to 12:00 PM" }
                     </p>
                     <p>
                       <b>Meal:</b> {booking.meal}
